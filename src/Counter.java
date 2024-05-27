679d91de07fd232ad7e3abd9040f84c487c96f8d
David Cohn <davidcohn929@gmail.com>
2024-05-27 17:57:32 -0400

    card counting strat

--- src/Counter.java
+++ src/Counter.java
@@ -1,3 +1,23 @@
 public class Counter {
-
+    public class Main {
+        public static void main(String[] args) {
+            int numberOfDecks = 6;
+            Deck deck = new Deck();
+            deck.shuffle();
+            CardCounter cardCounter = new CardCounter(numberOfDecks);
+    
+            for (int i = 0; i < 52 * numberOfDecks; i++) {
+                Card card = deck.dealCard();
+                if (card == null) {
+                    break;
+                }
+                cardCounter.updateCount(card);
+                System.out.println("Dealt: " + card + ", Running Count: " + cardCounter.getRunningCount());
+            }
+    
+            System.out.println("Final Running Count: " + cardCounter.getRunningCount());
+            System.out.println("True Count: " + cardCounter.getTrueCount());
+        }
+    }
+    
 }
