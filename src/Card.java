83596974825a1f461dce21e2cecc4e28d72dc240
Daniel Perodin <perodindaniel@gmail.com>
2024-05-27 17:33:49 -0400

    Made constructor for Card class, added ChatGPT's suggestions to the Deck class.

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,9 @@
 public class Card {
-
+    private String suit;
+    private String value;
+    public Card(String suit, String value)
+    {
+        this.suit = suit;
+        this.value = value;
+    }
 }
